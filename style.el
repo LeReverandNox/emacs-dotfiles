2bd670213100dbf8b8ad7b7953270dc4adbeed96
lrn <lrn@gmail.com>
2018-08-01 03:19:13 +0200

    Borrow some @hrs code to manage font ! Thanks !

--- style.el
+++ style.el
@@ -25,3 +25,49 @@
 
 ;; Disable the welcome screen
 (setq inhibit-startup-screen t)
+
+;; Stuff to manage the font ! Thanks @hrs
+(setq hrs/default-font "RobotoMono Nerd Font")
+(setq hrs/default-font-size 13)
+(setq hrs/current-font-size hrs/default-font-size)
+
+(setq hrs/font-change-increment 1.05)
+
+(defun hrs/font-code ()
+  "Return a string representing the current font (like \"Inconsolata-14\")."
+  (concat hrs/default-font "-" (number-to-string hrs/current-font-size)))
+
+(defun hrs/set-font-size ()
+  "Set the font to `hrs/default-font' at `hrs/current-font-size'.
+Set that for the current frame, and also make it the default for
+other, future frames."
+  (let ((font-code (hrs/font-code)))
+    (add-to-list 'default-frame-alist (cons 'font font-code))
+    (set-frame-font font-code)))
+
+(defun hrs/reset-font-size ()
+  "Change font size back to `hrs/default-font-size'."
+  (interactive)
+  (setq hrs/current-font-size hrs/default-font-size)
+  (hrs/set-font-size))
+
+(defun hrs/increase-font-size ()
+  "Increase current font size by a factor of `hrs/font-change-increment'."
+  (interactive)
+  (setq hrs/current-font-size
+        (ceiling (* hrs/current-font-size hrs/font-change-increment)))
+  (hrs/set-font-size))
+
+(defun hrs/decrease-font-size ()
+  "Decrease current font size by a factor of `hrs/font-change-increment', down to a minimum size of 1."
+  (interactive)
+  (setq hrs/current-font-size
+        (max 1
+             (floor (/ hrs/current-font-size hrs/font-change-increment))))
+  (hrs/set-font-size))
+
+(define-key global-map (kbd "C-=") 'hrs/reset-font-size)
+(define-key global-map (kbd "C-+") 'hrs/increase-font-size)
+(define-key global-map (kbd "C--") 'hrs/decrease-font-size)
+
+(hrs/reset-font-size)
