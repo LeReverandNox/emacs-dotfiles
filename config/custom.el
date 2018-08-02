bdb3d731961cc8208b2d28ce56a125a234ba5a46
lrn <lrn@gmail.com>
2018-08-02 03:14:21 +0200

    Update package list in custom.el

--- config/custom.el
+++ config/custom.el
@@ -12,7 +12,7 @@
  '(global-evil-surround-mode t)
  '(package-selected-packages
    (quote
-    (diminish evil-magit git-gutter-fringe git-gutter magit evil-mc multiple-cursors helm-descbinds helm powerline evil))))
+    (ag projectile rainbow-delimiters paredit diminish evil-magit git-gutter-fringe git-gutter magit evil-mc multiple-cursors helm-descbinds helm powerline evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
