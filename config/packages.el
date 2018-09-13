1caf62706898080533bdfa7c93ef663130139ad1
lereverandnox <lereverandnox@gmail.com>
2018-09-13 02:48:44 +0200

    Add pyenv-mode package to use pyenv within Emacs

--- config/packages.el
+++ config/packages.el
@@ -159,6 +159,12 @@
 
 ;; Python packages
 (use-package py-autopep8)
+(use-package pyenv-mode
+  :init
+  (add-to-list 'exec-path "~/.pyenv/shims")
+  (setenv "WORKON_HOME" "~/.pyenv/versions/")
+  :config
+  (pyenv-mode))
 (use-package elpy
   :config
   (elpy-enable)
