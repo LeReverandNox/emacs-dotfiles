d41c26684ec9b7e1926bd64fa0a21c1e21f710dd
lereverandnox <lereverandnox@gmail.com>
2018-10-13 15:40:17 +0200

    Add conf from 42 to have 42 headers !

--- init.el
+++ init.el
@@ -10,6 +10,12 @@
 ;; Load style relative settings
 (load "~/.emacs.d/config/style.el")
 
+;; Load the 42 header config
+(load "~/.emacs.d/config/42/list.el")
+(load "~/.emacs.d/config/42/string.el")
+(load "~/.emacs.d/config/42/comments.el")
+(load "~/.emacs.d/config/42/header.el")
+
 ;; Specify a file for customize to write into...
 (setq custom-file "~/.emacs.d/config/custom.el")
 ;; ...and load it at startup
