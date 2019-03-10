dc723783de3a1389252b7da257e33fc75ff0502a
lereverandnox <lereverandnox@gmail.com>
2019-03-11 00:22:18 +0100

    Update custom.el

--- config/custom.el
+++ config/custom.el
@@ -3,21 +3,28 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(custom-enabled-themes (quote (earthsong)))
  '(custom-safe-themes
-   (quote
-    ("59f42f7028e0cf1d8d6a7bc172dc37da64e4cd11fd29cf03c6ca950451cc2a00" default)))
+   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "adf5275cc3264f0a938d97ded007c82913906fc6cd64458eaae6853f6be287ce" default))
+ '(eyebrowse-mode t)
  '(global-evil-surround-mode t)
+ '(nil nil t)
+ '(org-agenda-files nil)
  '(package-selected-packages
-   (quote
-    (powerline-evil exec-path-from-shell pipenv cmake-mode cmake-ide flycheck-rtags company-rtags helm-rtags rtags irony-eldoc gist helm-projectile helm-swoop guide-key miniedit move-text move-lines autopair pyenv-mode elpy origami vimish-fold highlight-indent-guides smart-shift yaml-mode all-the-icons neotree flycheck company ox-twbs ox-beamer ox-md ox-latex htmlize org-beautify-theme org-bullets ag projectile rainbow-delimiters paredit diminish evil-magit git-gutter-fringe git-gutter magit evil-mc multiple-cursors helm-descbinds helm powerline evil))))
+   '(php-mode solarized-theme ag free-keys ivy-yasnippet go-guru go-flycheck go-eldoc company-go go-mode ivy-rtags aggressive-indent smart-region ivy-hydra sx speed-type evil-vimish-fold ivy-historian counsel-projectile counsel isearch docker-compose-mode dockerfile-mode smart-mode-line-powerline-theme smart-mode-line eyebrowse persp-projectile persp-mode which-key tide json-mode company-tern js2-refactor xref-js2 js2-mode company-quickhelp ibuffer-vc ialign persistent-scratch dired-narrow dired-quick-sort multi-term shell-pop dired+ emmet-mode web-mode ace-jump-mode evil-collection hideshow-org expand-region powerline-evil exec-path-from-shell pipenv cmake-mode cmake-ide flycheck-rtags company-rtags rtags irony-eldoc gist guide-key miniedit move-text move-lines autopair pyenv-mode elpy origami highlight-indent-guides smart-shift yaml-mode all-the-icons neotree flycheck company ox-twbs ox-beamer ox-md ox-latex htmlize org-beautify-theme org-bullets rainbow-delimiters paredit diminish evil-magit git-gutter-fringe git-gutter magit evil-mc multiple-cursors powerline))
+ '(tetris-x-colors
+   [[229 192 123]
+    [97 175 239]
+    [209 154 102]
+    [224 108 117]
+    [152 195 121]
+    [198 120 221]
+    [86 182 194]]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(eyebrowse-mode-line-active ((t (:inherit font-lock-builtin-face :weight bold))))
  '(org-archived ((t (:inherit org-meta-line))))
  '(org-block ((t (:inherit default))))
  '(org-code ((t (:inherit org-meta-line))))
